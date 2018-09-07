e670b6f3fd1ef56e4d75674cf63295cfec8a698b
Revath S Kumar <rsk@revathskumar.com>
2018-09-07 11:39:44 +0530

    feat(axios): use axios instead of request

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const fs = require('fs');
 
-const request = require('request');
+// const request = require('request');
+const axios = require('axios');
+const FormData = require('form-data');
 const puppeteer = require('puppeteer');
 const debug = require('debug')('diffity:screenshoter');
 
@@ -10,7 +12,7 @@ class Diffity {
     this.setOptions(options);
   }
 
-  setOptions(opts = {}) {
+  async setOptions(opts = {}) {
     const options = {
       os: 'linux',
       browser: 'chrome',
@@ -33,10 +35,22 @@ class Diffity {
     this.currentRunId = null;
 
     this.uploadQueue = [];
-    this.browser = await puppeteer.launch();
-    this.page =  await browser.newPage();
+    // this.browser = await puppeteer.launch();
+    // this.page =  await this.browser.newPage();
+
+    // debug( 'browser launched ');
+  }
+
+  async start() {
+    this.headlessBrowser = await puppeteer.launch();
+    this.page =  await this.headlessBrowser.newPage();
 
     debug( 'browser launched ');
+    return this;
+  }
+
+  end() {
+    this.headlessBrowser.close();
   }
 
   _validateProps() {
@@ -60,6 +74,10 @@ class Diffity {
     this.identifier = identifier;
     this._validateProps();
 
+    if (!this.page) {
+      await this.start();
+    }
+
     // const browser = await puppeteer.launch();
     // const page = await browser.newPage();
     // debug( 'browser launched ');
@@ -76,9 +94,9 @@ class Diffity {
     await this.page.screenshot({path: outputPath, fullPage: true});
 
     debug( 'screenshot saved ', outputPath);
-    this.uploadQueue.push(outputPath);
+    // this.uploadQueue.push(outputPath);
 
-    // this.upload(outputPath);
+    this.upload(outputPath);
 
     // browser.close();
     return this;
@@ -91,83 +109,127 @@ class Diffity {
   }
 
   createRun(details = {}) {
-    if (!details.project) {
-      details.project = this.projectName;
+    const runDetails = {
+      project: this.projectName,
+      name: `${details.project || this.projectName}-${new Date()}`,
+      js_driver: 'diffity-node',
+      group: 'group',
+      author: 'author',
+      ...details,
     }
-    if (!details.name) {
-      details.name = `${details.project}-${new Date()}`;
-    }
-    debug( 'creating run with name ', details.name);
-    const query = `project=${details.project}&name=${details.name}&js_driver=${details.js_driver || 'diffity-node'}&group=${details.group || 'group'}&author=${details.author || 'author'}`;
-    return new Promise((resolve, reject) => {
-      request
-        .post({url: this.apiBaseUrl + `/api/v1/runs?${query}`}, function optionalCallback(err, httpResponse, body) {
-          if (err) {
-            console.error('create run failed:', err);
-            return reject(new Error("Create Run Failed"));
+
+    debug( 'creating run with name ', runDetails.name);
+    // const query = `project=${details.project}&name=${details.name}&js_driver=${details.js_driver || 'diffity-node'}&group=${details.group || 'group'}&author=${details.author || 'author'}`;
+    // return new Promise((resolve, reject) => {
+    return axios
+        .post(this.apiBaseUrl + `/api/v1/runs`, runDetails, {
+          auth: {
+            username: this.apiKey, 
+            password: 'X'
           }
-          const response = JSON.parse(body);
-          if(response.id) {
-            debug('Run created :: ', response.id);
-            return resolve(response.id);
-          }
-          debug('Run create failed :: ', errors);
-          return reject(errors);
         })
-        .auth(this.apiKey, 'X', false);
-      });
+        .then((response) => {
+          if(response.data.id) {
+            debug('Run created :: ', response.data.id);
+            return response.data;
+          }
+        })
+        // .catch(errors => {
+
+        // })
+        //   if (err) {
+        //     console.error('create run failed:', err);
+        //     return reject(new Error("Create Run Failed"));
+        //   }
+        //   const response = JSON.parse(body);
+        //   if(response.id) {
+        //     debug('Run created :: ', response.id);
+        //     return resolve(response.id);
+        //   }
+        //   debug('Run create failed :: ', errors);
+        //   return reject(errors);
+        // })
+        // .auth(this.apiKey, 'X', false);
+      // });
   }
 
   async upload(screenshotPath) {
-    const formData = {
-      identifier: this.identifier,
-      image: {
-        value: fs.createReadStream(screenshotPath),
-        options: {
-          contentLength: 0,
-          contentType: "image/png"
-        }
-      },
+    // const formData = {
+    //   identifier: this.identifier,
+      // image: {
+      //   value: fs.createReadStream(screenshotPath),
+      //   options: {
+      //     contentLength: 0,
+      //     contentType: "image/png"
+      //   }
+      // },
       // attachments: [
       //   fs.createReadStream(screenshotPath)
       // ],
-      browser: this.browser,
-      device: this.device,
-      os: this.os,
+      // browser: this.browser,
+      // device: this.device,
+      // os: this.os,
       // browser_version: ,
-      device_name: this.deviceName,
+      // device_name: this.deviceName,
       // os_version:
       // multipart: [
       //   {body: fs.createReadStream(screenshotPath)},
       // ]
-    };
+    // };
 
     if (!this.currentRunId) {
-      this.currentRunId = await this.createRun();
+      const { id } = await this.createRun();
+      this.currentRunId = id; 
     }
 
-    debug( 'uploding screenshot ', screenshotPath);
-    return new Promise((resolve, reject) =>{
-      request
-        .post({url: this.apiBaseUrl + `/api/v1/runs/${this.currentRunId}/run_images`, formData: formData}, function optionalCallback(err, httpResponse, body) {
-          if (err) {
-            console.error('upload failed:', err);
-            return reject(new Error('Upload Failed'));
-          }
-          debug(body);
-          try {
-            const response = JSON.parse(body);
+    const formData = new FormData();
+    // formData.append("identifier", this.identifier);
+    // formData.append("browser", this.browser);
+    // formData.append("device", this.device);
+    // formData.append("os", this.os);
+    // formData.append("device_name", this.deviceName);
+    formData.append("image", fs.createReadStream(screenshotPath));
 
-            if(response.data) {
-              debug('Upload successful!  Server responded with:', body);
-              return resolve(response);
-            }
-            debug('Upload failed!  Server responded with:', body);
-            return reject(response);
-          } catch(e) {}
+    const queryParams = `identifier=${this.identifier}&browser=${this.browser}&device=${this.device}&os=${this.os}&device_name=${this,this.deviceName}`;
+
+    debug( 'uploading screenshot ', screenshotPath);
+    // return new Promise((resolve, reject) =>{
+      // request
+      axios
+        .post(this.apiBaseUrl + `/api/v1/runs/${this.currentRunId}/run_images?${queryParams}`, formData, {
+          headers:{'Content-Type': 'mulipart/form-data; charset=utf-8', 'Accept': 'application/json'},
+          auth: {
+            username: this.apiKey, 
+            password: 'X'
+          }
         })
-        .auth(this.apiKey, 'X', false);
-    })
+        .then(response => {
+          if(response.data.id) {
+            debug('Upload successful!  Server responded with:', response.data);
+            return response.data;
+          }
+          debug('Upload failed!  Server responded with:', response.data);
+          return response.data;
+        })
+
+        //   if (err) {
+        //     console.error('upload failed:', err);
+        //     return reject(new Error('Upload Failed'));
+        //   }
+        //   debug(body);
+        //   try {
+        //     const response = JSON.parse(body);
+
+        //     if(response.data) {
+        //       debug('Upload successful!  Server responded with:', body);
+        //       return resolve(response);
+        //     }
+        //     debug('Upload failed!  Server responded with:', body);
+        //     return reject(response);
+        //   } catch(e) {}
+        // })
+        // .auth(this.apiKey, 'X', false);
+    // })
 
   }
 }
